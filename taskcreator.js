51bfbbafed566fe41fd172b1c572489ad2956900
Andrija Petrovic <andrija.hers@gmail.com>
2016-09-06 21:07:46 +0000

    Important function sinkNameFromString added to execSuite

--- taskcreator.js
+++ taskcreator.js
@@ -3,6 +3,25 @@ function createTasks(execlib) {
 
   var sinkhunters = require('./tasks/sinkhunterscreator')(execlib);
 
+  function sinkNameFromStringSegment (sseg, index, arry) {
+    var colonindex, namerole = 'user', subsinkname = sseg.trim();
+    if (index === arry.length-1) {
+      return subsinkname;
+    }
+    colonindex = subsinkname.indexOf(':');
+    if (colonindex>0) {
+      namerole = subsinkname.substr(colonindex+1);
+      subsinkname = subsinkname.substr(0, colonindex);
+    }
+    return {name: subsinkname, identity: {name: namerole, role: namerole}};
+  }
+
+  function sinkNameFromString (string) {
+    return string.indexOf(',') > 0 ? string.split(',').map(sinkNameFromStringSegment) : string;
+  }
+
+  execlib.execSuite.sinkNameFromString = sinkNameFromString;
+
   return [{
     name: 'findSink',
     klass: require('./tasks/findSink')(execlib, sinkhunters)
